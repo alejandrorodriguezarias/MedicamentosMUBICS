af8cb097038f705be8716c6573816abc6a3aec2d
Alex <alejandro.rodriguez.arias@udc.es>
2019-12-19 17:59:35 +0100

    For para los smiles

--- Medicamentos.R
+++ Medicamentos.R
@@ -3,17 +3,22 @@ library(readxl)
 library(Rcpi)
 #Filtramos la base de datos para tener solo los valores referentes a IC50 y Homo Sapiens
 dataset <- read_excel("dataset_Chembl_Cytokines.xlsx")
-tmp <- dataset$CANONICAL_SMILES
-tmp <- data.frame("smiles" = dataset$CANONICAL_SMILES, "type" = dataset$STANDARD_TYPE_UNITSj, "value" = dataset$STANDARD_VALUE, "organism" = dataset$ASSAY_ORGANISM)
-tmp<- filter(tmp,tmp$type=="IC50 (nM)")
-tmp<- filter(tmp,tmp$organism=="Homo sapiens")
-tmp
+medicamentos <- dataset$CANONICAL_SMILES
+medicamentos <- data.frame("smiles" = dataset$CANONICAL_SMILES, "type" = dataset$STANDARD_TYPE_UNITSj, "value" = dataset$STANDARD_VALUE, "organism" = dataset$ASSAY_ORGANISM)
+medicamentos<- filter(medicamentos,medicamentos$type=="IC50 (nM)")
+medicamentos<- filter(medicamentos,medicamentos$organism=="Homo sapiens")
+medicamentos
 
-
+#obtenemos los smiles
+smiles <- medicamentos$smiles
+vectorDescriptores <- data.frame()
 library(rcdk)
-anle138b = parse.smiles("C1OC2=C(O1)C=C(C=C2)C3=CC(=NN3)C4=CC(=CC=C4)Br")
-descriptors = get.desc.names(type="all")
-descriptorsvalues = eval.desc(anle138b,descriptors)
-             
+for(i in 1:length(smiles)) {
+  anle138b = parse.smiles(as.vector(smiles[i]))
+  descriptors = get.desc.names(type="all")
+  descriptorsvalues = eval.desc(anle138b,descriptors)
+  descriptorsvalues
+  vectorDescriptores <- rbind(vectorDescriptores,descriptorsvalues)
+}
     
              
