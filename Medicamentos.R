065241cda9bd9c21eca36580b62ac01815c2d2b2
Alex <alejandro.rodriguez.arias@udc.es>
2019-12-19 17:14:12 +0100

    Base de datos y uso invididual de rcdk

--- Medicamentos.R
+++ Medicamentos.R
@@ -1,6 +1,7 @@
 library(dplyr)
 library(readxl)
 library(Rcpi)
+#Filtramos la base de datos para tener solo los valores referentes a IC50 y Homo Sapiens
 dataset <- read_excel("dataset_Chembl_Cytokines.xlsx")
 tmp <- dataset$CANONICAL_SMILES
 tmp <- data.frame("smiles" = dataset$CANONICAL_SMILES, "type" = dataset$STANDARD_TYPE_UNITSj, "value" = dataset$STANDARD_VALUE, "organism" = dataset$ASSAY_ORGANISM)
@@ -8,35 +9,11 @@ tmp<- filter(tmp,tmp$type=="IC50 (nM)")
 tmp<- filter(tmp,tmp$organism=="Homo sapiens")
 tmp
 
-x <- readMolFromSDF("C:/Users/Alex/Desktop/master/prueba.sdf")
-extractDrugALOGP(x.mol)
-y <- readMolFromSmi("C:/Users/Alex/Desktop/master/Bioinformatic/smiles.smi")
-#View(dataset_Chembl_Cytokines)
-tmp[4]
-install.packages("xlsx")
-library(xlsx)
-write.xlsx(tmp$smiles, "C:/Users/Alex/Desktop/master/smiles.xlsx")
 
-extractDrugALOGP(x)
-extractDrugAIO(x)
-
-library(ChemmineOB)
-
-if (!requireNamespace("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-
-BiocManager::install("ChemmineR")
-
-library(ChemmineR)
-
-n <-smiles2sdf(as.character(tmp[1,1,1]))
-y <- convertFormat("SMI","SDF",tmp[1,1,1])
-
-extractDrugALOGP(n[1])
-extractDrugAIO(n)
-
-sdfset <- read.SDFset("C:/Users/Alex/Desktop/master/prueba.sdf") 
-
-conn <- initDb("test.db")
-ids<-loadSdf(conn, sdfset, function(sdfset) data.frame(rings(sdfset,type="count",upper=6, arom=TRUE))
+library(rcdk)
+anle138b = parse.smiles("C1OC2=C(O1)C=C(C=C2)C3=CC(=NN3)C4=CC(=CC=C4)Br")
+descriptors = get.desc.names(type="all")
+descriptorsvalues = eval.desc(anle138b,descriptors)
+             
+    
              
